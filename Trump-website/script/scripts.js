3766e6d19046eb3d72b106769cd378132b12ea0a
Arin Ibrahim <ai23019@auis.edu.krd>
2026-05-28 17:24:53 +0300

    Delete Trump-website directory

--- Trump-website/script/scripts.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  const nav = document.querySelector('.main-nav');
-  nav.classList.toggle('open');
-  console.log('Toggled menu, open:', nav.classList.contains('open'));
-});
